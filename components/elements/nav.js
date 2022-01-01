55bfb7cbfaf58812e3de91639b0a562566c35ced
Jason Matthews <matthews.jason@mixemapp.com>
2021-12-31 23:26:46 -0500

    logo take to home page

--- components/elements/nav.js
+++ components/elements/nav.js
@@ -1,13 +1,12 @@
-/* This example requires Tailwind CSS v2.0+ */
-import { Fragment } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
+import { Disclosure } from "@headlessui/react";
+import { useRouter } from "next/router";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
 const Example = () => {
+  const router = useRouter();
   return (
     <Disclosure as="nav" className="bg-white shadow">
       {({ open }) => (
@@ -16,9 +15,11 @@ const Example = () => {
             <div className="relative flex justify-between h-16">
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex-shrink-0 flex items-center">
-                  <p className=" font-bold border-2 border-black p-1">
-                    LUX TAX CALCULATOR
-                  </p>
+                  <button onClick={() => router.push("/")}>
+                    <p className=" font-bold border-2 border-black p-1">
+                      LUX TAX CALCULATOR
+                    </p>
+                  </button>
                 </div>
               </div>
             </div>
