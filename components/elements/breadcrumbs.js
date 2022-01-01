37e360895fc97b85fd6cb2e985e24e0bcc4d2e27
Jason Matthews <matthews.jason@mixemapp.com>
2021-12-31 23:03:55 -0500

    Extra bold text for selected bread crumb

--- components/elements/breadcrumbs.js
+++ components/elements/breadcrumbs.js
@@ -54,7 +54,7 @@ const BreadCrumbs = ({ active }) => {
                 <a
                   className={`text-sm font-medium text-gray-500 hover:text-gray-700 ${
                     index !== 0 && "ml-4 "
-                  } ${index === active && "text-canadaRed font-bold"} ${
+                  } ${index === active && "text-canadaRed font-extrabold"} ${
                     page.finished && "text-canadaRed "
                   }`}
                   aria-current={index === active ? "page" : undefined}
